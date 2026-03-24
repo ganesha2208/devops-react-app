7311922ab16e9df206dec4b2208b282b3628a77d
ganesha2208 <ganeshadream11@gmail.com>
2026-03-24 22:21:50 +0530

    fi

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{textAlign: "center", marginTop: "50px"}}>
+      <h1>🚀 DevOps CI/CD Project</h1>
+      <p>Deployed using Docker, AWS & GitHub Actions</p>
+    </div>
+  );
+}
+
+export default App;
